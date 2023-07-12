3b3ee55f58ac9857a66c8e9899c5dce515e95dc4
alfarabygit <alfarabyindra@gmail.com>
2023-07-12 21:55:30 +0700

    upload projek web music dengan html css dan js

---
+++ assets/js/music-list.js
@@ -0,0 +1,14 @@
+let allMusic = [
+  {
+    name: "Cinta ini Membunuhku", //music name
+    artist: "D-Masiv", //artist name
+    img: "music-1", //image name
+    src: "music-1", //name here
+  },
+  {
+    name: "Lumpuhkan Ingatanku",
+    artist: "Geisha",
+    img: "music-2",
+    src: "music-2",
+  },
+];
